5485c650b317b35b27a6fbc54d41668df5f45cd1
AWTMedia <alexander.thomas2401@gmail.com>
2026-03-04 09:16:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,11 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-          <Route path="/audit" element={<Audit />} />
-          <Route path="/audit-confirmation" element={<AuditConfirmation />} />
-        </Routes>
+  <Route path="/" element={<Index />} />
+  <Route path="/audit" element={<Audit />} />
+  <Route path="/audit-confirmation" element={<AuditConfirmation />} />
+  <Route path="*" element={<NotFound />} />
+</Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
